223b522fefc7dbb6078f95a857b1b1086d815174
eileendong <eileendong1@gmail.com>
2022-07-26 22:37:26 -0500

    more changes

--- test-main/app.js
+++ test-main/app.js
@@ -1,3 +1,5 @@
+/* mobile drop down menu*/
+
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
@@ -5,3 +7,15 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+/* hide sroll bar*/
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
